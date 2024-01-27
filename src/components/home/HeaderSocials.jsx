db35295d0d217fbe30888446d5008a30260586a5
BFSupavida <F.Supavida@gmail.com>
2024-01-27 23:38:11 +0700

    add Home page

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export const HeaderSocials = () => {
   return (
     <div className="home__socials"> 
-        <a href="" className="home__social-link" target='_blank'>
+        <a href="https://www.linkedin.com/in/supavida-itthirak-1a6a77207/" className="home__social-link" target='_blank'>
             <i class="fa-brands fa-linkedin"></i>
         </a>
 
-        <a href="" className="home__social-link" target='_blank'>
-        <i class="fa-brands fa-square-github"></i>
+        <a href="https://github.com/BFSupavida" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-square-github"></i>
         </a>
 
         
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import "./home.css"
 import Me from "../../assets/avatar-1.svg"
 import { HeaderSocials } from './HeaderSocials'
+import { ScrollDown } from './ScrollDown'
+import { Shapes } from './Shapes'
 
 
 export const Home = () => {
@@ -13,7 +15,12 @@ export const Home = () => {
         <span className="home__education">I'm a Full-stack developer</span>
         <p className='status'>Open to work</p>
         <HeaderSocials />
+        <a href="#contact" className="btn">Hire Me!</a>
+        <ScrollDown />
+
       </div>
+
+      <Shapes />
     </section>
   )
 }
