bbc26e7f81e4905fbf7f79419eb519c8d4a0b717
BFSupavida <F.Supavida@gmail.com>
2024-02-09 23:53:00 +0700

    update(Contact.jsx): fixed css path

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Sidebar } from "../sidebar/Sidebar";
-import "./Contact.css"
+import "./contact.css";
 
 export function Contact() {
   const onSubmit = async (event) => {
@@ -16,33 +16,40 @@ export function Contact() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: json
+      body: json,
     }).then((res) => res.json());
 
     if (res.success) {
       console.log("Success", res);
     }
+    
   };
 
   return (
     <section className="contact container section">
-    <Sidebar />
-    <h2 className="section__title flex-container">Contact</h2>
-    <div className="contact_form">
-      <p></p>
-      <form className="" onSubmit={onSubmit}>
-        <p className="topic_request">Name</p>
-        <input className="input_request" type="text" name="name"/>
-        <p className="topic_request">Email</p>
-        <input className="input_request" type="email" name="email"/>
-        <p className="topic_request">More details as you wish</p>
-        <textarea className="input_request_textarea" name="message"></textarea>
-        <br />
-        <div className="box_btn"><button className="btn" type="submit">Submit Form</button></div>
-        
-      </form>
+      <Sidebar />
+      <h2 className="section__title flex-container">Contact</h2>
+      <div className="contact_form">
+        <p></p>
+        <form className="" onSubmit={onSubmit}>
+          <p className="topic_request">Name</p>
+          <input className="input_request" type="text" name="name" />
+          <p className="topic_request">Email</p>
+          <input className="input_request" type="email" name="email" />
+          <p className="topic_request">More details as you wish</p>
+          <textarea
+            className="input_request_textarea"
+            name="message"
+          ></textarea>
+          <br />
+          <div className="box_btn">
+            <button className="btn" type="submit">
+              Submit Form
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import LightLogo from "../../assets/Bai_Logo.png";
 
-
 import {
   RiHome2Line,
   RiUser3Line,
@@ -17,51 +16,53 @@ export const Sidebar = (props) => {
 
   return (
     <>
-      <aside className={toggle ? "aside show-menu" : "aside"}>
-        <a href={"/"} className="nav__logo">
-          <img className="logo-1" src={LightLogo} alt="logo" />
-        </a>
+      <div className="box-sidebar">
+        <aside className={toggle ? "aside show-menu" : "aside"}>
+          <a href={"/"} className="nav__logo">
+            <img className="logo-1" src={LightLogo} alt="logo" />
+          </a>
 
-        <nav className="nav">
-          <div className="nav__menu">
-            <ul className="nav__list">
-              <li className="nav__item">
-                <a href={"/"} className="nav__link">
-                  <RiHome2Line />
-                </a>
-              </li>
+          <nav className="nav">
+            <div className="nav__menu">
+              <ul className="nav__list">
+                <li className="nav__item">
+                  <a href={"/"} className="nav__link">
+                    <RiHome2Line />
+                  </a>
+                </li>
 
-              <li className="nav__item">
-                <a href={'/about'} className="nav__link">
-                  <RiUser3Line />
-                </a>
-              </li>
+                <li className="nav__item">
+                  <a href={"/about"} className="nav__link">
+                    <RiUser3Line />
+                  </a>
+                </li>
 
-              <li className="nav__item">
-                <a href={'/skills'} className="nav__link">
-                  <RiFileList3Line />
-                </a>
-              </li>
+                <li className="nav__item">
+                  <a href={"/skills"} className="nav__link">
+                    <RiFileList3Line />
+                  </a>
+                </li>
 
-              <li className="nav__item">
-                <a href={"/project"} className="nav__link">
-                  <RiStackLine />
-                </a>
-              </li>
+                <li className="nav__item">
+                  <a href={"/project"} className="nav__link">
+                    <RiStackLine />
+                  </a>
+                </li>
 
-              <li className="nav__item">
-                <a href={"/contact"} className="nav__link">
-                  <RiChat3Line />
-                </a>
-              </li>
-            </ul>
+                <li className="nav__item">
+                  <a href={"/contact"} className="nav__link">
+                    <RiChat3Line />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className="nav__footer">
+            <span className="copyright">&copy; 2024</span>
           </div>
-        </nav>
-
-        <div className="nav__footer">
-        <span className="copyright">&copy; 2024</span>
-        </div>
-      </aside>
+        </aside>
+      </div>
 
       <div
         className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"}
