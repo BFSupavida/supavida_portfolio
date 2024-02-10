27bf3d81728d622f4638153b4e4f4c22c91753d6
BFSupavida <F.Supavida@gmail.com>
2024-02-10 15:35:37 +0700

    change path of icon (Skills.jsx)

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -9,7 +9,12 @@ import img_node from "../../../public/assets/iconskills/icons8-nodejs-100.svg";
 import img_express from "../../../public/assets/iconskills/icons8-express-js.svg";
 import img_boostrap from "../../../public/assets/iconskills/icons8-bootstrap.svg";
 import img_tailwind from "../../../public/assets/iconskills/icons8-tailwind-css.svg";
-
+import img_mongo from "../../../public/assets/iconskills/icons8-mongodb.svg";
+import img_github from "../../../public/assets/iconskills/icons8-github.svg";
+import img_figma from "../../../public/assets/iconskills/icons8-figma.svg";
+import img_trello from "../../../public/assets/iconskills/icons8-trello.svg";
+import img_comptia from "../../../public/assets/iconskills/comptia-logo.webp"
+import img_codecademy from "../../../public/assets/iconskills/icon-codecademy.png"  
 
 export const Skills = () => {
   return (
@@ -23,58 +28,58 @@ export const Skills = () => {
           <p>HTML</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-css3.svg" alt="icon-css" />
+          <img src={img_css} alt="icon-css" />
           <p>CSS</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-javascript-100.svg" alt="icon-javascript" />
+          <img src={img_javascript} alt="icon-javascript" />
           <p>JavaScript</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-react.svg" alt="icon-react" />
+          <img src={img_react} alt="icon-react" />
           <p>React</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-nodejs-100.svg" alt="icon-node" />
+          <img src={img_node} alt="icon-node" />
           <p>Node.js</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-express-js.svg" alt="icon-expressjs" />
+          <img src={img_express} alt="icon-expressjs" />
           <p>ExpressJS</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-bootstrap.svg" alt="icon-boostrap" />
+          <img src={img_boostrap} alt="icon-boostrap" />
           <p>Boostrap</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-tailwind-css.svg" alt="icon-html" />
+          <img src={img_tailwind} alt="icon-html" />
           <p>Tailwind</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-mongodb.svg" alt="icon-html" />
+          <img src={img_mongo} alt="icon-html" />
           <p>MongoDB</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-github.svg" alt="icon-GitHub" />
+          <img src={img_github} alt="icon-GitHub" />
           <p>GitHub</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-figma.svg" alt="icon-Figma" />
+          <img src={img_figma} alt="icon-Figma" />
           <p>Figma</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icons8-trello.svg" alt="icon-trello" />
+          <img src={img_trello} alt="icon-trello" />
           <p>Trello</p>
         </div>
         </div>
 
         <h2 className="bullet">Certification</h2>
         <div className="certification-container"><div className="iconskills">
-          <img src="../../../public/assets/iconskills/comptia-logo.webp" alt="icon-trello" />
+          <img src={img_comptia} alt="icon-trello" />
           <p>CompTIA ITF+</p>
         </div>
         <div className="iconskills">
-          <img src="../../../public/assets/iconskills/icon-codecademy.png" alt="icon-trello" />
+          <img src={img_codecademy} alt="icon-trello" />
           <p>Learn HTML Course</p>
         </div></div>
         <h2 className="bullet">Languages</h2>
