91275849dd73e09459027601724814058244ac31
BFSupavida <F.Supavida@gmail.com>
2024-02-11 11:50:28 +0700

    fixed alert (Contact.jsx)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sidebar } from "../sidebar/Sidebar";
 import "./contact.css";
 import { Shapes } from "../home/Shapes";
+import light from "../../../public/assets/light/price-1.png"
 
 export function Contact() {
+  //set emty of input
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -23,7 +31,12 @@ export function Contact() {
     }).then((res) => res.json());
 
     if (res.success) {
-      console.log("Success", res);
+      alert("Thank you for your contact");
+      setFormData({
+        name: '',
+        email: '',
+        message: ''
+      });
     }
   };
 
@@ -32,21 +45,32 @@ export function Contact() {
       <Sidebar />
       <section className="contact container section">
         <h2 className="section__title flex-container">Contact</h2>
+        <div className="before_contact_form">
+          <img className="light_bulb" src={light} alt="light_bulb" />
+          <div>
+            <h3>If you want to know more about me ?</h3>
+            <h3>Let's get in on</h3>
+          </div>
+          
+        </div>
+        
         <div className="contact_form">
-          <p></p>
+          
           <form className="" onSubmit={onSubmit}>
             <p className="topic_request">Name</p>
-            <input className="input_request" type="text" name="name" />
+            <input className="input_request" type="text" name="name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
             <p className="topic_request">Email</p>
-            <input className="input_request" type="email" name="email" />
+            <input className="input_request" type="email" name="email"  value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
             <p className="topic_request">More details as you wish</p>
             <textarea
               className="input_request_textarea"
               name="message"
+              value={formData.message}
+              onChange={(e) => setFormData({ ...formData, message: e.target.value })}
             ></textarea>
             <br />
             <div className="box_btn">
-              <button className="btn" type="submit">
+              <button className="btn" type="submit" >
                 Submit Form
               </button>
             </div>
